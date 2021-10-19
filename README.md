59b2c0c9cbe15415b67a69f64fdf0edff623a9cb
Christos Zonios <czonios@gmail.com>
2021-10-20 00:46:28 +0300

    connect to supercollider

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # cyclical-dance
+
+## Prerequisites
+
+### Arduino libraries
+
+- Arduino Nano 33 BLE boards
+- ArduinoBLE
+- Arduino_LSM9DS1
+
+### Python libraries
+
+- pygatt
+
+one of:
+
+- pyosc3
+- python-osc
--- controller/supercollider.py
+++ controller/supercollider.py
@@ -1,8 +1,10 @@
 import pyOSC3
+# import pythonosc
 
 def init_supercollider():
     client = pyOSC3.OSCClient()
-    client.connect(( '127.0.0.1', 57120 ))
+    client.connect('127.0.0.1', 57110)
+    # client = pythonosc.udp_client.SimpleUDPClient('127.0.0.1', 57110)
     return client
 
 def send_pos(position, sc):
@@ -11,6 +13,22 @@ def send_pos(position, sc):
     msg.setAddress(f"/{position}")
     msg.append(500)
     sc.send(msg)
+    # sc.send_message(f"/{position}", 0)
 
 def reset_sc(sc):
-    raise NotImplementedError
+    send_pos("reset", sc)
+
+if __name__ == "__main__":
+    sc = init_supercollider()
+    import time
+    send_pos(1, sc)
+    time.sleep(0.5)
+    send_pos(2, sc)
+    time.sleep(0.5)
+    send_pos(3, sc)
+    time.sleep(0.5)
+    send_pos(4, sc)
+    time.sleep(0.5)
+    send_pos(5, sc)
+    time.sleep(0.5)
+    reset_sc(sc)
