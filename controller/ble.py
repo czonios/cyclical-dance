bb8250f1c54b121af24dfd2100c7d7bcaa540478
Christos Zonios <czonios@gmail.com>
2021-10-22 13:16:58 +0300

    works fine on windows

---
+++ controller/ble.py
@@ -0,0 +1,50 @@
+import time
+import pygatt
+import asyncio
+from bleak import BleakClient
+
+def connect_ble_linux(MAC_ADDR):
+    adapter = pygatt.GATTToolBackend()
+
+    # Start the adapter
+    adapter.start(reset=False)
+    
+    # Connect to the device with that given parameter.
+    device = adapter.connect(MAC_ADDR, timeout=1000)
+    time.sleep(0.1)
+    return device, adapter
+
+def subscribe_ble_linux(MAC_ADDR, BLE_UUID_DATA, data_handler_cb):
+    device, adapter = connect_ble_linux()
+    # subscribe to data characteristic
+    device.subscribe(BLE_UUID_DATA,
+                callback=data_handler_cb)
+    input("Press enter to stop program...\n")
+    adapter.stop()
+
+async def connect_ble_windows(MAC_ADDR, BLE_UUID_DATA, data_handler_cb):
+    client = BleakClient(MAC_ADDR)
+    try:
+        print("Awaiting connection...")
+        await client.connect(timeout=1000)
+        asyncio.sleep(0.1)
+        print("Connected to BLE")
+        await client.start_notify(BLE_UUID_DATA, data_handler_cb)
+        while 1:
+            if not client.is_connected:
+                break
+            # d = await client.read_gatt_char(BLE_UUID_DATA, use_cached=False)
+            # data_handler_cb(None, d)
+            await asyncio.sleep(5.0, loop=asyncio.get_event_loop())
+        print("Connection to BLE broken")
+        await client.stop_notify(BLE_UUID_DATA)
+        await client.disconnect()
+    except Exception as e:
+        print("An error occured in BLE connection:", e)
+    finally:
+        await client.disconnect()
+
+
+
+def subscribe_ble_windows(MAC_ADDR, BLE_UUID_DATA, data_handler_cb):
+    asyncio.run(connect_ble_windows(MAC_ADDR, BLE_UUID_DATA, data_handler_cb))
--- controller/main.py
+++ controller/main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
-import time
-import pygatt
 import os
+from ble import subscribe_ble_linux, subscribe_ble_windows
 from supercollider import *
 
 LUT = ["RESET", "TOP_RIGHT", "TOP", "TOP_LEFT", "BOTTOM_LEFT", "BOTTOM", "BOTTOM_RIGHT", "INVALID"]
@@ -10,9 +9,6 @@ BLE_UUID_DATA_SERVICE = "2BEEF31A-B10D-271C-C9EA-35D865C1F48A"
 BLE_UUID_DATA =         "4664E7A1-5A13-BFFF-4636-7D0A4B16496C"
 MAC_ADDR =              "c6:d3:74:98:41:c8"
 supercollider = None
-
-def triggerEvent(checkdata):
-   print(str(checkdata))
 
 def data_handler_cb(handle, value):
     """
@@ -28,16 +24,7 @@ def data_handler_cb(handle, value):
     else:
         supercollider.send_pos(position)
 
-def connect_ble():
-    adapter = pygatt.GATTToolBackend()
 
-    # Start the adapter
-    adapter.start(reset=False)
-    
-    # Connect to the device with that given parameter.
-    device = adapter.connect(MAC_ADDR, timeout=1000)
-    time.sleep(0.1)
-    return device, adapter
 
 def do_reset():
     # send reset to SC
@@ -56,33 +43,11 @@ def main():
     try:
         # connect to Arduino
         if os.name == 'nt':
-            print("Windows...")
-            import asyncio
-            from bleak import BleakClient
-
-
-            async def test():
-                async with BleakClient(MAC_ADDR) as client:
-                    await asyncio.sleep(0.1)
-                    print(f"Connected: {client.is_connected}")
-                    while 1:
-                        # d = await client.read_gatt_char(BLE_UUID_DATA)
-                        # print('d:', d)
-                        # data_handler_cb(None, d)
-                        await client.start_notify(BLE_UUID_DATA, data_handler_cb)
-                    input("Press enter to stop program...\n")
-                    await client.stop_notify(BLE_UUID_DATA)
-
-            asyncio.run(test())
+            subscribe_ble_windows(MAC_ADDR, BLE_UUID_DATA, data_handler_cb)
         else:
-            device, adapter = connect_ble()
-            # subscribe to data characteristic
-            device.subscribe(BLE_UUID_DATA,
-                        callback=data_handler_cb)
-            input("Press enter to stop program...\n")
-            adapter.stop()
-    finally:
-        pass
+            subscribe_ble_linux(MAC_ADDR, BLE_UUID_DATA, data_handler_cb)
+    except Exception as e:
+        print("An error occured:", e)
         
 
 if __name__ == "__main__":
